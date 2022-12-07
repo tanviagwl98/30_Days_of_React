f602511c5b29abb023c013864a1fc1108a714748
Tanvi Agarwal <45534001+tanviagwl98@users.noreply.github.com>
2022-12-07 10:43:38 +0530

    Update Hooks.js

--- 30_days_of_react/src/Components/Hooks.js
+++ 30_days_of_react/src/Components/Hooks.js
@@ -1,5 +1,9 @@
 //useQueryParam
-
+/*
+  1. useQueryParam: It eases encoding and decoding data of any type as query parameters
+  with smart memoization to prevent creating unnecessary duplicate objects. All query parameters
+  should be encoded as strings.
+*/
 import {useQueryParam} from 'beautiful-react-hooks';
 import {Input} from 'beautiful-react-ui';
 import {HashRouter} from 'react-router-dom';
